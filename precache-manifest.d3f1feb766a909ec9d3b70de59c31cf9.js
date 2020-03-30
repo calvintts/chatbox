a00ffc57bda0faa808cffbb04bb5c0611d102a72
calvintts <calvinteytaisoon@gmail.com>
2020-03-30 15:52:28 -0700

    Updates

---
+++ precache-manifest.d3f1feb766a909ec9d3b70de59c31cf9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a9f94fdaad675f4b5db48755592bdba9",
+    "url": "/chatbox/index.html"
+  },
+  {
+    "revision": "dae1cd587bfab115d6e2",
+    "url": "/chatbox/static/js/2.4c51cabd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/chatbox/static/js/2.4c51cabd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6e49adba0c48314afe38",
+    "url": "/chatbox/static/js/main.ff30eeed.chunk.js"
+  },
+  {
+    "revision": "ab6f60b71b4b566c62f4",
+    "url": "/chatbox/static/js/runtime-main.075a8dc2.js"
+  },
+  {
+    "revision": "2ecddf2ebeb58d72dc12330651d9cec3",
+    "url": "/chatbox/static/media/calvin.2ecddf2e.jfif"
+  },
+  {
+    "revision": "0ff43bb7ac035ff419e5e979d5b45db0",
+    "url": "/chatbox/static/media/steph.0ff43bb7.jfif"
+  }
+]);
